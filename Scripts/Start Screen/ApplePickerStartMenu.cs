6b47b4f772fdfefebef6244625edbedfd3228c10
brandonassing <assing.brandon@gmail.com>
2016-02-24 21:57:51 -0500

    First Commit

---
+++ Scripts/Start Screen/ApplePickerStartMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ApplePickerStartMenu : MonoBehaviour
+{
+    public Button easyButton, medButton, hardButton, exitButton;
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        easyButton.GetComponentInChildren<Text>().text = "Easy";
+        medButton.GetComponentInChildren<Text>().text = "Medium";
+        hardButton.GetComponentInChildren<Text>().text = "Hard";
+        exitButton.GetComponentInChildren<Text>().text = "Exit";
+    }
+    public void EasyStart()
+    {
+        Application.LoadLevel("_Scene_0");
+    }
+    public void MediumStart()
+    {
+        Application.LoadLevel("_Scene_1");
+    }
+    public void HardStart()
+    {
+        Application.LoadLevel("_Scene_2");
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit(); 
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
